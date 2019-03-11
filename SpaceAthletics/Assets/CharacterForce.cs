2a3791330b6f44ee35d18f5fedadc33cf5e40c21
NottyAce <39573156+NottyAce@users.noreply.github.com>
2019-03-11 10:35:08 +0900

    ひとまず仮セーブ

---
+++ SpaceAthletics/Assets/CharacterForce.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterForce : MonoBehaviour {
+
+    [SerializeField]
+    GameObject player;//キャラのインスタンス
+    Rigidbody playerRigidbody;//キャラのRigidbodyのインスタンス
+    Transform playerTransform;//Transformのインスタンス
+    Vector3 playerPos;/*= new Vector3();*///キャラの座標
+    [SerializeField]
+    float g;//キャラに加える力
+
+	// Use this for initialization
+	void Start () {
+        playerRigidbody = player.GetComponent<Rigidbody>();//インスタンス格納
+        playerTransform = player.transform;//〃
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        playerPos = CharacterMove();
+        CharacterStandingManager(playerPos);
+	}
+
+    private Vector3 CharacterMove()//キャラ移動（仮置き）
+    {
+        Vector3 Movement = new Vector3(0,0,1);
+        playerRigidbody.AddForce(Movement * g);
+
+        return playerTransform.position;
+    }
+
+    private void CharacterStandingManager(Vector3 playerPos)//姿勢制御
+    {
+
+    }
+
+}
--- SpaceAthletics/Assets/GravityController.cs
+++ SpaceAthletics/Assets/GravityController.cs
@@ -4,38 +4,52 @@ using UnityEngine;
 
 public class GravityController : MonoBehaviour {
 
-    public GameObject player;
-    Rigidbody playerRigidbody;
-    Rigidbody planetRigidbody;
+    public GameObject player;//プレイヤーのインスタンス
+    public Rigidbody playerRigidbody;//プレイヤーのRigidbody
+    Rigidbody planetRigidbody;//惑星のRigidbody
     public float g;
-    Vector3 pos;
-    Vector3 basePos;
+    Transform playerTransform;
+    Transform planetTransform;
+    public Vector3 normalVector;
+    //Vector3 playerPos;//プレイヤーの座標
+    //Vector3 basePos;//惑星の座標
     
     // Use this for initialization
 	void Start ()
     {
-        playerRigidbody = player.GetComponent<Rigidbody>();
+        playerRigidbody = player.GetComponent<Rigidbody>();//以下全てインスタンス格納
         planetRigidbody = GetComponent<Rigidbody>();
+        playerTransform = player.GetComponent<Transform>();
+        planetTransform = GetComponent<Transform>();
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-		
+        normalVector = GravityVector(planetTransform, planetTransform);
 	}
 
     private void FixedUpdate()
     {
-        GravityManager();
+        GravityManager(normalVector);
     }
 
-    private void GravityManager()
+    public Vector3 GravityVector(Transform playerTransform,Transform planetTransform)
     {
-        pos = player.transform.position;
-        basePos = transform.position;
+        Vector3 playerPos = playerTransform.position;//引数のTransformから座標を入手
+        Vector3 basePos = planetTransform.position;//〃
+        Vector3 normalVector = basePos - playerPos;//法線ベクトルを計算
+        normalVector = normalVector.normalized;//算出した法線ベクトルを単位ベクトルに変換
+   
+        return normalVector;//戻り値として法線単位ベクトルを返す
+    }
 
-        Vector3 gravityDirection = basePos - pos;
-        Vector3 gravityScaler = g * gravityDirection.normalized * (planetRigidbody.mass * playerRigidbody.mass) / (gravityDirection.sqrMagnitude);
+
+    private void GravityManager(Vector3 normalVector)
+    {
+
+
+        Vector3 gravityScaler = g * normalVector * (planetRigidbody.mass * playerRigidbody.mass) / (normalVector.sqrMagnitude);
         playerRigidbody.AddForce(gravityScaler);
     }
 }
